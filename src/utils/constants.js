5abf708439637761adec49d2a62b31df5c703e4b
Srikanth4533 <kondakallasrikanth991@gmail.com>
2022-09-02 10:23:39 +0530

    added folder structure and made some changes

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+
+
+export const darkTheme = {
+    mainFont: '#FFF',
+    primary: '#1F2128',
+    secondary: '#191B1F', // darkest
+    secondaryInverted: '#FFF',
+    tertiary: '#2C2F36',
+    barChart: '#2172e5',
+    buttonClose: 'rgb(6, 213, 84)',
+    lineChart: '#00ff5f',
+    lineChartGradientTop: '#414547',
+    lineChartGradientBottom: '#000',
+    hrLineTop: '#707070',
+    hrLineBottom: '#646565',
+}
+  
+export const lightTheme = {
+    mainFont: '#000',
+    primary: '#f7f7f7',
+    secondary: '#FFF',
+    secondaryInverted: '#191B1F',
+    tertiary: '#edeff2',
+    barChart: '#1ad761',
+    buttonClose: '#000',
+    lineChart: '#2550ea',
+    lineChartGradientTop: '#6785f0',
+    lineChartGradientBottom: '#FFF',
+    hrLineTop: '#e4e4e4',
+    hrLineBottom: '#f0f0f0',
+}
